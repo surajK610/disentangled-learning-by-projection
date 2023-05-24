9e0597ffaa20e7c88173e478bfd89e2ac947aca8
suraj_anand@brown.edu <surajK610@gmail.com>
2023-05-24 13:15:14 -0700

    fixed device bug

--- models/logistic_classifier.py
+++ models/logistic_classifier.py
@@ -4,9 +4,10 @@ from tqdm import tqdm
 import numpy as np
 
 class LogisticClassifier(nn.Module):
-    def __init__(self, input_features, classes, lr=0.001):
+    def __init__(self, input_features, classes, device, lr=0.001):
         super().__init__()
-        self.linear = nn.Linear(input_features, classes).to(device)
+        self.device = device
+        self.linear = nn.Linear(input_features, classes).to(self.device)
 
         self.optimizer = torch.optim.AdamW(self.linear.parameters(), lr=lr)
         self.loss_fn = torch.nn.CrossEntropyLoss()
@@ -18,7 +19,7 @@ class LogisticClassifier(nn.Module):
     def acc(self, y_pred, target):
         return torch.mean(torch.eq(y_pred, target).float()).item()
 
-    def fit(self, dataloader, val_dataloader=None, encoder=None, label='number', device='cuda:0', epochs=50, evaluate_every=1):
+    def fit(self, dataloader, val_dataloader=None, encoder=None, label='number', epochs=50, evaluate_every=1):
         all_losses = []
         val_losses = []
         pbar = tqdm(range(int(epochs)), desc='Training Epochs')
@@ -26,12 +27,12 @@ class LogisticClassifier(nn.Module):
         val_acc = np.nan
         for i in pbar:
             for image_batch, labels in dataloader:
-                image_batch = image_batch.to(device)
+                image_batch = image_batch.to(self.device)
                 if encoder is not None: 
                     image_batch = encoder(image_batch)
                     
                 labels = labels[label]
-                labels = labels.to(device)
+                labels = labels.to(self.device)
                 
                 self.optimizer.zero_grad()
                 y_pred = self.forward(image_batch)
@@ -44,12 +45,12 @@ class LogisticClassifier(nn.Module):
                 acc_batches = []
                 loss_batches = []
                 for image_batch, labels in val_dataloader:
-                    image_batch = image_batch.to(device)
+                    image_batch = image_batch.to(self.device)
                     if encoder is not None: 
                         image_batch = encoder(image_batch)
 
                     labels = labels[label]
-                    labels = labels.to(device)
+                    labels = labels.to(self.device)
                     with torch.no_grad():
                         y_pred = self.forward(image_batch)
                         val_loss = self.loss_fn(y_pred, labels)
